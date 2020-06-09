f9c3a9b95f77e3d318e4be24ed9f4de6f76cf610
Diego Covarrubias <dcov@pm.me>
2020-06-08 19:10:26 -0700

    Rename write_file to write_path, Refactor write_path to return a Path instead of file

--- src/write_path.rs
+++ src/write_path.rs
@@ -1,9 +1,11 @@
-//! Write file lookup.
+//! Write path lookup.
 //!
 //! This module contains the lookup functionality for the '.gitignore' file that
 //! will be written to.
+//!
+//! Note: It returns the path to the file only, it does not open any files.
 
-use std::fs::{self, File, DirEntry};
+use std::fs::{self, DirEntry};
 use std::path::PathBuf;
 
 fn search_dir_for_gitignore_path(dir_path: &PathBuf, only_git_root: bool) -> Option<PathBuf> {
@@ -42,11 +44,11 @@ fn search_dir_for_gitignore_path(dir_path: &PathBuf, only_git_root: bool) -> Opt
     None
 }
 
-fn find_gitignore_path(from_dir_path: PathBuf, only_git_root: bool) -> Option<PathBuf> {
-    let mut current_dir_path = from_dir_path;
+pub fn lookup(from_dir_path: &PathBuf, only_git_root: bool) -> PathBuf {
+    let mut current_dir_path = from_dir_path.clone();
     loop {
         if let Some(path) = search_dir_for_gitignore_path(&current_dir_path, only_git_root) {
-            return Some(path);
+            return path;
         }
 
         if !only_git_root || !current_dir_path.pop() {
@@ -54,94 +56,67 @@ fn find_gitignore_path(from_dir_path: PathBuf, only_git_root: bool) -> Option<Pa
         }
     }
 
-    None
-}
-
-/// Opens and returns the .gitignore file that should be written to.
-pub fn open(from_dir_path: PathBuf, only_git_root: bool) -> Result<File, ()> {
-    if let Some(path) = find_gitignore_path(from_dir_path, only_git_root) {
-        if let Ok(file) = File::open(path.clone()) {
-            return Ok(file);
-        }
-
-        if let Ok(file) = File::create(path.clone()) {
-            return Ok(file);
-        }
-    }
-
-    Err(())
+    panic!("Could not find .gitignore write file.");
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+    use std::panic::catch_unwind;
     use tempfile;
 
     #[test]
-    fn test_find_gitignore_path() {
+    fn test_lookup() {
         let root_dir = tempfile::tempdir().unwrap();
         let root_dir_path = root_dir.path().to_path_buf();
 
         // Add some random files into the directory
-        File::create(root_dir_path.join("random.txt")).unwrap();
-        File::create(root_dir_path.join("fizz.h")).unwrap();
-        File::create(root_dir_path.join("buzz.c")).unwrap();
+        fs::File::create(root_dir_path.join("random.txt")).unwrap();
+        fs::File::create(root_dir_path.join("fizz.h")).unwrap();
+        fs::File::create(root_dir_path.join("buzz.c")).unwrap();
 
         // Assert that searching for a .gitignore file path when [only_git_root] is [false]
         // will return a path when there is no .gitignore file in the directory.
-        let result = find_gitignore_path(root_dir_path.clone(), false);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        assert_eq!(lookup(&root_dir_path, false), root_dir_path.join(".gitignore"));
 
         // Assert that searching for a .gitignore file path when [only_git_root] is [false]
         // will return a path when there is a .gitignore file in the directory.
-        File::create(root_dir_path.join(".gitignore")).unwrap();
-        let result = find_gitignore_path(root_dir_path.clone(), false);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        fs::File::create(root_dir_path.join(".gitignore")).unwrap();
+        assert_eq!(lookup(&root_dir_path, false), root_dir_path.join(".gitignore"));
 
         // Assert that searching for a .gitignore file path when [only_git_root] is true
         // will not return a path if there is no .git directory, even if there is a .gitignore
         // file.
-        let result = find_gitignore_path(root_dir_path.clone(), true);
-        assert!(result.is_none());
+        assert!(catch_unwind(|| lookup(&root_dir_path, true)).is_err());
 
         // Assert that searching for a .gitignore file path when [only_git_root] is true
         // will return a path if there is a .git directory, when there is no .gitignore
         // file.
         fs::create_dir(root_dir_path.join(".git")).unwrap();
         fs::remove_file(root_dir_path.join(".gitignore")).unwrap();
-        let result = find_gitignore_path(root_dir_path.clone(), true);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        assert_eq!(lookup(&root_dir_path, true), root_dir_path.join(".gitignore"));
 
         // Assert that searching for a .gitignore file path when [only_git_root] is true
         // will return a path if there is a .git directory, when there is a .gitignore
         // file.
-        File::create(root_dir_path.join(".gitignore")).unwrap();
-        let result = find_gitignore_path(root_dir_path.clone(), true);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        fs::File::create(root_dir_path.join(".gitignore")).unwrap();
+        assert_eq!(lookup(&root_dir_path, true), root_dir_path.join(".gitignore"));
 
         // Assert that searching for a .gitignore file when [only_git_root] is true
         // will return a path if a parent dir has a .git directory, when there is no .gitignore
         // file.
         let sub_dir_path = root_dir_path.join("subdir");
         fs::create_dir(sub_dir_path.clone()).unwrap();
-        File::create(sub_dir_path.join("sub.txt")).unwrap();
+        fs::File::create(sub_dir_path.join("sub.txt")).unwrap();
         fs::remove_file(root_dir_path.join(".gitignore")).unwrap();
-        let result = find_gitignore_path(sub_dir_path.clone(), true);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        assert_eq!(lookup(&sub_dir_path, true), root_dir_path.join(".gitignore"));
 
         // Assert that searching for a .gitignore file when [only_git_root] is true
         // will return a path if a parent dir has a .git directory, when there is a .gitignore
         // file.
-        File::create(root_dir_path.join(".gitignore")).unwrap();
-        let result = find_gitignore_path(sub_dir_path.clone(), true);
-        assert!(result.is_some());
-        assert_eq!(result.unwrap(), root_dir_path.join(".gitignore"));
+        fs::File::create(root_dir_path.join(".gitignore")).unwrap();
+        assert_eq!(lookup(&sub_dir_path, true), root_dir_path.join(".gitignore"));
 
         root_dir.close().unwrap();
     }
